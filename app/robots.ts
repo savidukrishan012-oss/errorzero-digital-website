a6510e13d62274c1ad7772fd215eb77d3bd4d12b
K.D.SAVINDU KRISHAN WEERAKKODI <krishansavindu923@gmail.com>
2026-01-07 18:31:11 +0530

    Add robots.txt and sitemap.xml for SEO

--- app/robots.ts
+++ app/robots.ts
@@ -1,9 +1,11 @@
-import { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
+export default function robots() {
   return {
-    rules: [{ userAgent: "*", allow: "/" }],
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
     sitemap: "https://zyraxe.com/sitemap.xml",
   };
 }
-
