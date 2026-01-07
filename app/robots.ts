754a684270d49a5cfaf0b6e519230d0216ed1b96
K.D.SAVINDU KRISHAN WEERAKKODI <krishansavindu923@gmail.com>
2026-01-07 18:10:11 +0530

    Add Google Search Console verification

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,7 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [{ userAgent: "*", allow: "/" }],
     sitemap: "https://zyraxe.com/sitemap.xml",
   };
 }
